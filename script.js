202b8d6180b24292a3689d2763b11abe4371235d
Giannis Alexiou <45979358+y1ann1s85@users.noreply.github.com>
2019-02-11 18:45:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,201 @@
+// 1
+// let hello_english = "Hello World! // English";
+// let hello_italian = "Ciao, mondo! // Italian";
+// let hello_spanish = "Hola, mundo! // Spanish";
+
+// console.log(hello_english, hello_italian, hello_spanish);
+
+// 2
+// console.log("I'm awesome");
+
+// 3
+
+// 3.1
+// let x;
+
+// 3.2
+// console.log("I think the type of x will be: ", typeof x);
+
+// 3.3
+// console.log(x);
+
+// 3.4
+// x = 5;
+
+// 3.5
+// console.log("I think the type of x will be: ", typeof x);
+
+// 3.6
+// console.log(x);
+
+// 4
+// let y = "";
+
+// 4.1
+// console.log("I think the value of y will be: ", typeof y);
+
+// 4.2
+// console.log(y);
+
+// 4.3
+// y = "Hello";
+
+// 4.4
+// console.log("I think the value of y will be: ", typeof y);
+
+// 4.5
+// console.log(y);
+
+// 5
+
+// 5.1
+// let z = 7.25;
+
+// 5.2
+// console.log(z);
+
+// 5.3
+// let a = Math.round(z);
+
+// 5.4
+// console.log(a);
+
+// 5.5
+// let u;
+
+// if (a > z) {
+//     u = a;
+// } else {
+//     u = z;
+    
+// }
+
+// 5.6
+// console.log(u);
+
+// 6
+
+// 6.1
+// let plurality = [];
+
+// 6.2
+// console.log("I think the type of plurality is: ", typeof plurality);
+
+// 6.3
+// console.log(plurality);
+
+// 6.4
+// let my_perfect_animals = ["dragons"];
+
+// 6.5
+// console.log(my_perfect_animals);
+
+// 6.6
+// my_perfect_animals.push("baby pig");
+
+// 6.7
+// console.log(my_perfect_animals);
+
+// 7.2
+// let myString = "this is a test";
+// console.log(myString);
+
+// 7.3
+// myString.length;
+
+// 7.4
+// console.log(myString.length);
+
+// 8
+
+// 8.1
+// let e = {};
+// let s = "";
+// let d = 1;
+// let w = false;
+
+// 8.2
+// console.log("The value of my variable e is: ", e);
+// console.log("The value of my variable s is: ", s);
+// console.log("The value of my variable d is: ", d);
+// console.log("The value of my variable w is: ", w);
+
+// 8.3-8.4
+// console.log("The value of my variable e is: ", typeof e);
+// console.log("The value of my variable s is: ", typeof s);
+// console.log("The value of my variable d is: ", typeof d);
+// console.log("The value of my variable w is: ", typeof w);
+
+// 8.5
+// if (e !== s) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// if (e !== d) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// if (e !== w) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// if (s !== d) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// if (s !== w) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// if (d !== w) {
+//   console.log("Different type");  
+// } else {
+//   console.log("Same type");
+// }
+
+// 9
+// let x1 = 7;
+// x1 = x1 % 3;
+// console.log(x1);
+
+// 9.1
+// console.log("In computing, the modulo operation finds the remainder after division of one number by another (sometimes called modulus)");
+// console.log("5 % 2 would evaluate to 1 because 5 divided by 2 leaves a quotient of 2 and a remainder of 1");
+// console.log("9 % 3 would evaluate to 0 because the division of 9 by 3 has a quotient of 3 and leaves a remainder of 0");
+
+// 10
+
+// 10.1
+// console.log("You can store multiple types in an array!");
+
+// let myArray = [];
+// myArray [0] = "Giannis";
+// myArray [1] = 34;
+// myArray [2] = true;
+
+// console.log(myArray);
+
+// 10.2
+// 6/0 === 10/0;
+
+// let y1 = 6/0;
+// let y2 = 10/0;
+
+// if (y1 === y2) {
+//   console.log("true")
+// } else {
+//   console.log("false")
+// }
+
+//  10.3
+// console.log("The triple equals sign (===) is used to compare both the Type and the actual Value of a variable");
